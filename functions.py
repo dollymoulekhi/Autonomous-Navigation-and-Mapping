b546ceaf081a1cc9d3eaed87fc9541e6a188c363
Dolly Moulekhi <dollymoulekhi97@gmail.com>
2024-01-07 22:05:51 -0500

    changes in the file, calling it as functions.py

--- functions.py
+++ functions.py
@@ -15,7 +15,19 @@ def color_thresh(image, rgb_thresh=(0, 0, 0)):
 
     color_select[above_tresh]=1
     return color_select
+
+
+
+## defination of the function
+
+
+def perspect_transform(image, source, dst):
+    transform_mat = cv2.getPerspectiveTransform(source, dst)
+    destination_image = cv2.warpPerspective(image, transform_mat, (320, 160))
+    return destination_image
+
     
+## calling of the function
 red_threshold = 160
 green_threshold = 160
 blue_threshold = 160
@@ -26,18 +38,9 @@ rgb_threshold = (red_threshold, green_threshold, blue_threshold)
 colorsel = color_thresh(image, rgb_thresh=rgb_threshold)
 
 
-
-
-###############################################################################
-def perspect_transform(image, source, dst):
-    transform_mat = cv2.getPerspectiveTransform(source, dst)
-    destination_image = cv2.warpPerspective(image, transform_mat, (320, 160))
-    return destination_image
-
-
+## calling of the function perspective transformation
 dst_size = 5 
 bottom_offset = 6
-
 
 source = np.float32([[14, 140], [301 ,140],[200, 96], [118, 96]])
 destination = np.float32([[image.shape[1]/2 - dst_size, image.shape[0] - bottom_offset],
@@ -50,10 +53,15 @@ destination = np.float32([[image.shape[1]/2 - dst_size, image.shape[0] - bottom_
 warped=perspect_transform(image, source, destination)
 
 
+
+
+## drawining polygon 
 cv2.polylines(image, np.int32([source]), True, (0, 0, 255), 3)
 cv2.polylines(warped, np.int32([destination]), True, (0, 0, 255), 3)
 
 
+
+## displaying the image 
 
 f, (ax1, ax2) = plt.subplots(1, 2, figsize=(24, 6), sharey=True)
 f.tight_layout()
@@ -63,9 +71,6 @@ ax1.set_title('Original Image', fontsize=40)
 ax2.imshow(warped, cmap='gray')
 ax2.set_title('Result', fontsize=40)
 plt.subplots_adjust(left=0., right=1, top=0.9, bottom=0.)
-
-###############################################################################
-
 
 # Display the original image and binary               
 f, (ax1, ax2) = plt.subplots(1, 2, figsize=(21, 7), sharey=True)
