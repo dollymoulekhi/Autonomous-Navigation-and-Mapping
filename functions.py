875727409eedf3b7f25ff51fc9496552866d126e
Dolly Moulekhi <dollymoulekhi97@gmail.com>
2024-01-07 23:47:53 -0500

    transforing the map from rovers prospective to worlds perspective, by using tanslational and rotational matrix

--- functions.py
+++ functions.py
@@ -26,6 +26,17 @@ def perspect_transform(image, source, dst):
     destination_image = cv2.warpPerspective(image, transform_mat, (image.shape[1], image.shape[0]))
     return destination_image
 
+
+def rover_coords(binary_img):
+    # Identify nonzero pixels
+    ypos, xpos = binary_img.nonzero()
+    # Calculate pixel positions with reference to the rover position being at the 
+    # center bottom of the image.  
+    x_pixel = np.absolute(ypos - binary_img.shape[0]).astype(float)
+    y_pixel = -(xpos - binary_img.shape[0]).astype(float)
+    return x_pixel, y_pixel
+
+
     
 ## calling of the function
 red_threshold = 160
